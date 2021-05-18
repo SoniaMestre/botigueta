7b01670daa1ead6e237443c2a7282d6c10d13010
Sonia Mestre <mestre.sonia@gmail.com>
2021-05-18 18:55:23 +0200

    creacio projecte

---
+++ src/main/java/org/factoria5/store/BotiguetaApplication.java
@@ -0,0 +1,13 @@
+package org.factoria5.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotiguetaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotiguetaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/factoria5/store/BotiguetaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoria5.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotiguetaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
